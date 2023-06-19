1bacc7f17287c41d3da200b9bbe3a8840dde97ac
Ali <a.samy@prog.com>
2023-06-19 13:39:27 +0300

    changed style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import MovieCard from './components/MovieCard.vue'
+import MovieShowCard from './components/MovieShowCard.vue'
 </script>
 
 <template>
@@ -21,11 +21,20 @@ import MovieCard from './components/MovieCard.vue'
       <div>
         <h2>Now Playing</h2>
       </div>
-      <div class="cards">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+      <div class="swiper"></div>
+    </section>
+
+    <section id="popular-movies" class="container">
+      <h2>Popular Movies</h2>
+      <div class="grid">
+        <MovieShowCard />
+        <MovieShowCard />
+        <MovieShowCard />
+        <MovieShowCard />
+        <MovieShowCard />
+        <MovieShowCard />
+        <MovieShowCard />
+        <MovieShowCard />
       </div>
     </section>
   </main>
@@ -33,65 +42,32 @@ import MovieCard from './components/MovieCard.vue'
 
 <style scoped>
 .container {
-  max-width: 1100px;
+  max-width: 1200px;
   margin-left: auto;
   margin-right: auto;
 }
 
-#now-playing {
-  padding: 16px;
-  margin-top: 20px;
-  background-color: #656565;
-}
-
-#now-playing .cards {
-  display: flex;
-}
-
-#search {
-  margin-top: 20px;
-  padding: 24px;
-  background-color: #656565;
+section h2 {
+  margin: 20px 0;
   text-align: center;
+  text-transform: uppercase;
 }
 
-header form {
-  padding: 10px;
+/* Grid */
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 20px;
 }
 
-header form input {
-  width: 70%;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 8px 0 0 8px;
-}
-
-header form button {
-  border: none;
-  padding: 10px 20px;
-  border-radius: 0 8px 8px 0;
-  color: white;
-  background-color: rgb(41, 147, 98);
-  cursor: pointer;
-}
-
-#search form button:active {
-  border: 1px solid #000000;
-}
-
-#now-playing div h2 {
-  text-align: center;
-  padding: 30px;
-}
-
+/* Heading */
 header {
   height: 80px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #656565;
+  background-color: #000000c0;
   padding: 12px;
-  margin-top: 30px;
 }
 
 header h1 a {
@@ -125,5 +101,56 @@ header nav a:hover {
 header nav a:active {
   background-color: #979797;
   color: rgb(41, 147, 98);
+}
+
+#search {
+  margin-top: 20px;
+  padding: 24px;
+  background-color: #656565;
+  text-align: center;
+}
+
+header form {
+  padding: 10px;
+}
+
+header form input {
+  width: 70%;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 8px 0 0 8px;
+}
+
+header form button {
+  border: none;
+  padding: 10px 20px;
+  border-radius: 0 8px 8px 0;
+  color: white;
+  background-color: rgb(41, 147, 98);
+  cursor: pointer;
+}
+
+#search form button:active {
+  border: 1px solid #000000;
+}
+
+/* Now Playing */
+section#now-playing {
+  background: url(../src/images/showcase-bg.jpg) no-repeat center center/cover;
+  padding: 60px;
+}
+
+#now-playing .cards {
+  display: flex;
+}
+
+.swiper {
+  width: 100%;
+  height: 50%;
+}
+
+/* Popular Movies */
+section#popular-movies {
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/MovieShowCard.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <div class="card">
+    <div>
+      <a href="/">
+        <img src="../images/no-image.jpg" />
+      </a>
+    </div>
+    <div class="card-body">
+      <p>Fast X</p>
+      <p>Release Date: 2020-6-12</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background: #04376b;
+  padding: 5px;
+}
+
+.card img {
+  width: 100%;
+}
+
+.card-body {
+  padding: 10px;
+  font-size: 20px;
+}
+
+.card:hover {
+  transform: scale(1.05);
+  transition: all 0.5s ease-in-out;
+  background: #0a4b8f;
+}
+</style>
